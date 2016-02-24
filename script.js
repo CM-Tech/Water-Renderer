509ccde6d7ade92e1fdecec50863f748bc2bce92
modderme123 <modderme123@gmail.com>
2016-02-24 14:06:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// SPH
-// http://wonderfl.net/c/2pg0
-// を、JavaScript化したもの
+var colors = [
+    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+    "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"
+];
+var mouseX = 0;
+var mouseY = 0;
+var press = false;
 
-function whenClick(event) {
+document.getElementById("canvas").onmousedown = function(event) {
     clickControl(event);
 }
 
-function whenMove(event) {
+document.getElementById("canvas").onmousemove = function(event) {
     clickControl(event);
-}
+};
 
 function clickControl(event) {
 
@@ -25,10 +29,10 @@ var RANGE2;
 var w = $("body").width();
 var h = $("body").height();
 var SPH = {
-    GRAVITY: 0.0125,
-    RANGE: 10,
-    PRESSURE: 0.125,
-    VISCOSITY: 0.05
+    GRAVITY: 0.05,
+    RANGE: 20,
+    PRESSURE: 0.5,
+    VISCOSITY: 0.1
 };
 $("body").keydown(function(event) {
     console.log(event.which);
@@ -42,26 +46,30 @@ $("body").keydown(function(event) {
     }
 });
 var currentCenter;
-var mouseX = 0;
-var mouseY = 0;
-var press = false;
+
 var initialize = (function() {
     var col = 0;
     $("#canvas").attr("width", $("body").width());
     $("#canvas").attr("height", $("body").height());
-    clickControl({
+    /* clickControl({
             layerX: 0.5,
             layerY: 3,
             buttons: 1
-        });
+        });*/
     RANGE2 = SPH.RANGE * SPH.RANGE;
-    var DENSITY = 0.2;
+    var DENSITY = 1;
     var NUM_GRIDSX = Math.floor(w / 10);
     var NUM_GRIDSY = Math.floor(h / 10);
     var INV_GRID_SIZEX = 1 / (w / NUM_GRIDSX);
     var INV_GRID_SIZEY = 1 / (h / NUM_GRIDSY);
-    var canvas = document.getElementById('canvas');
+    var canvas2 = document.getElementById('canvas');
+    var ctx2 = canvas2.getContext('2d');
+    var canvas = document.createElement('canvas');
     var ctx = canvas.getContext('2d');
+    canvas2.width = $("body").width();
+    canvas2.height = $("body").height();
+    canvas.width = $("body").width();
+    canvas.height = $("body").height();
     var particles = [];
     var numParticles = 0;
     var neighbors = [];
@@ -77,15 +85,19 @@ var initialize = (function() {
 
     function frame(e) {
 
-        if (press)
-            pour();
+
 
         var tempDelta = delta + 0;
         delta = 0;
-calc();
+        //calc();
         ctx.clearRect(0, 0, w, h);
-        var d = draw();
-        ;
+        draw();
+        ctx2.clearRect(0, 0, w, h);
+        ctx2.drawImage(canvas,0,0);
+        //ctx2.clearRect(0, 0, w, h);
+        //ctx2.drawImage(canvas, 0, 0);
+        //ctx2.fill();
+
         ctx.font = "30px Arial";
         ctx.fillText("" + SPH.RANGE, 10, 30);
 
@@ -93,123 +105,46 @@ calc();
     }
 
     function draw() {
-        
-        ctx.beginPath();
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, w, h);
-        ctx.fill();
-        
+        drag();
+        //ctx2.fillRect(0,0,10,10);
+        // ctx2.fill();
+
+        //ctx.beginPath();
+        //ctx.fillStyle = "black";
+        //ctx.fillRect(0, 0, w, h);
+        //ctx.fill();
+
         ctx.globalCompositeOperation = 'normal';
         for (var i = 0; i < numParticles; i++) {
             var p = particles[i];
             //var grd = ctx.createRadialGradient(p.x, p.y, 0, p.x, p.y, p.size / 2);
 
-            //ctx.fillStyle = "orange";
+            ctx.fillStyle = "blue";
 
             ctx.beginPath();
-            //ctx.arc(p.x, p.y, p.size / 4, 0, 2 * Math.PI, false);
-            ctx.fillRect(p.x, p.y, 4, 4);
-            var color = HSVtoRGB(0.7, 1, 1.5 - Math.sqrt(p.density * 2));
+            ctx.arc(p.x, p.y, p.size / 8, 0, 2 * Math.PI, false);
+            //ctx.fillRect(p.x-2, p.y-2, 4, 4);
+            //var color = HSVtoRGB(0.7, 1, 1.5 - Math.sqrt(p.density * 2));
             //color=HSVtoRGB(0.6,1,p.size/50);
             //color = HSVtoRGB(Math.sqrt(p.density * 4), 1, 0.5);
             //ctx.fillStyle = "hsl(" + Math.sqrt(p.density * 4) * 360 + ",100%,50%)";
             //grd.addColorStop(0, "rgba(" + color.r + "," + color.g + "," + color.b + ",01)");
             //grd.addColorStop(1, "rgba(" + color.r + "," + color.g + "," + color.b + ",0)");
-            ctx.fillStyle = "blue";
+            //ctx.fillStyle = colors[i % 2];
 
             //maxDensity=Math.max(maxDensity,p.density);
 
             ctx.fill();
-            ctx.fillStyle = "blue";
-            ctx.beginPath();
+            //ctx.fillStyle = "blue";
+            //ctx.beginPath();
 
-            ctx.strokeStyle = "white";
-            ctx.moveTo(p.x, p.y);
-            currentCenter = p;
-            var dist = Math.sqrt(p.vx * p.vx + p.vy * p.vy);
+            //ctx.strokeStyle = "white";
+            //ctx.moveTo(p.x, p.y);
+            //currentCenter = p;
+            //var dist = Math.sqrt(p.vx * p.vx + p.vy * p.vy);
             //ctx.lineTo(p.x+p.vx*p.size/4,p.y+p.vy*p.size/4);
-            if (false) {
-                var paintNes = findPaintNeighbors(p);
-                if (paintNes.length < 2) {
-                    ctx.beginPath();
-                    ctx.arc(p.x, p.y, p.size / 4, 0, 2 * Math.PI, false);
-                    //var color = HSVtoRGB(0.7, 1, 1.5 - Math.sqrt(p.density * 2));
-                    //color=HSVtoRGB(0.6,1,p.size/50);
-                    //color = HSVtoRGB(Math.sqrt(p.density * 4), 1, 0.5);
-                    //ctx.fillStyle = "hsl(" + Math.sqrt(p.density * 4) * 360 + ",100%,50%)";
-                    //grd.addColorStop(0, "rgba(" + color.r + "," + color.g + "," + color.b + ",01)");
-                    //grd.addColorStop(1, "rgba(" + color.r + "," + color.g + "," + color.b + ",0)");
-                    ctx.fillStyle = "blue";
-
-                    //maxDensity=Math.max(maxDensity,p.density);
-
-                    ctx.fill();
-                }
-
-                var sideBiasX = 0;
-                var sideBiasY = 0;
-                var filledAngles = [];
-                for (var k = 0; k < 180; k++) {
-                    filledAngles[k] = 0;
-                }
-                for (var k = 0; k < paintNes.length; k++) {
-                    var a = paintNes[k];
-                    //var aV=0;
-                    var aV = Math.atan2(a.y - currentCenter.y, a.x - currentCenter.x) / Math.PI * 90;
-                    //for (var m = -45; m < 45; m++) {
-                    filledAngles[Math.floor((aV) % 180)] = 1;
-                    //}
-                }
-                var isEdge = false;
-                var countSince = 0;
-                for (var k = 0; k < 180; k++) {
-                    countSince++;
-                    if (filledAngles[k] == 1) {
-
-                        if (countSince > 70) {
-                            isEdge = true;
-                            break;
-                        }
-                        countSince = 0;
 
 
-                    }
-                }
-                if (countSince > 30) {
-                    isEdge = true;
-
-                }
-                /*for (var k = 0; k < paintNes.length; k++) {
-                var biasDist = Math.sqrt((paintNes[k].x - p.x) * (paintNes[k].x - p.x) + (paintNes[k].y - p.y) * (paintNes[k].y - p.y));
-                sideBiasX += (paintNes[k].x - p.x) / biasDist / paintNes.length;
-                sideBiasY += (paintNes[k].y - p.y) / biasDist / paintNes.length;
-            }*/
-                if (paintNes.length < 2 && isEdge == true) { //&& Math.sqrt(sideBiasX * sideBiasX + sideBiasY * sideBiasY) > 0.2) {
-                    ctx.arc(p.x, p.y, p.size / 4, 0, 2 * Math.PI, false);
-                    ctx.fillStyle = "lightblue";
-                    ctx.fill();
-                    paintNes = paintNes.sort(function(a, b) {
-                        var aV = Math.atan2(a.y - currentCenter.y, a.x - currentCenter.x);
-                        var bV = Math.atan2(b.y - currentCenter.y, b.x - currentCenter.x);
-                        if (aV > bV) {
-                            return -1;
-                        }
-                        if (bV > aV) {
-                            return 1;
-                        }
-                        // a must be equal to b
-                        return 0;
-                    });
-                    for (var k = 0; k < paintNes.length; k++) {
-                        var pk = paintNes[k];
-                        var pk2 = paintNes[(k + 1) % paintNes.length];
-                        ctx.moveTo(p.x, p.y);
-                        ctx.lineTo(pk.x, pk.y);
-                        ctx.lineTo(pk2.x, pk2.y);
-                    }
-                    ctx.fill();
-                }
-            }
             //ctx.stroke();
 
         }
@@ -234,28 +169,7 @@ calc();
 
         }*/
         ctx.globalCompositeOperation = 'normal';
-        /* for (var i = 0; i < neighbors.length; i++) {
-            var pair = neighbors[i];
-            ctx.beginPath();
-            ctx.lineCap="round";
-            var grd=ctx.createLinearGradient(pair.p1.x,pair.p1.y, pair.p2.x,pair.p2.y);
-            grd.addColorStop(0,"hsl("+Math.sqrt(pair.p1.density*4)*360+",100%,50%)");
-            grd.addColorStop(1,"hsl("+Math.sqrt(pair.p2.density*4)*360+",100%,50%)");
-            ctx.strokeStyle=grd;
-            var deg=Math.atan2(pair.nx, -pair.ny);
-            ctx.lineWidth=(250)/pair.distance;
-            ctx.moveTo(pair.p1.x-Math.cos(deg+Math.PI)*(pair.p1.size-ctx.lineWidth)/2,
-            pair.p1.y-Math.sin(deg+Math.PI)*(pair.p1.size-ctx.lineWidth)/2);
-            
-            
-            ctx.quadraticCurveTo(pair.p1.x/2+pair.p2.x/2,pair.p1.y/2+pair.p2.y/2,pair.p2.x-Math.cos(deg+Math.PI)*8, pair.p2.y-Math.sin(deg+Math.PI)*8);
-            //ctx.moveTo(pair.p1.x,pair.p1.y);
-            //ctx.lineTo(pair.p2.x,pair.p2.y);
-            ctx.stroke();
-        }*/
-        //ctx.restore();
-        //ctx.fillStyle="rgba(0,0,255,"+maxDensity*2+")";
-        //return maxDensity;
+
 
     }
 
@@ -277,12 +191,24 @@ calc();
     }
 
     function calc() {
-
+        if (press) {
+            pour();
+        }
         updateGrids();
         findNeighbors();
         calcPressure();
         calcForce();
-        move(4);
+        move(1);
+        updateGrids();
+        findNeighbors();
+        calcPressure();
+        calcForce();
+        move(1);
+        updateGrids();
+        findNeighbors();
+        calcPressure();
+        calcForce();
+        move(1);
     }
 
     function move(d) {
@@ -295,6 +221,19 @@ calc();
             for (var j = 0; j < d; j++) {
                 p.move();
             }
+        }
+    }
+
+    function drag() {
+        //count++;
+        //var i;
+        //var p;
+
+        for (var i = 0; i < numParticles; i++) {
+            var p = particles[i];
+            //for (var j = 0; j < d; j++) {
+            p.drag();
+            //}
         }
     }
 
@@ -417,8 +356,8 @@ calc();
             }
             pixelArray[x] = col;
         }
-        for (var y = 20; y < 600; y += 20) {
-            for (var x = 20; x < w - 20; x += 20) {
+        for (var y = 20; y < 400; y += 10) {
+            for (var x = w / 2 - w/4; x < w / 2 + w/4; x += 10) {
                 var p = new Particle(x, y);
                 p.vy = 0;
                 particles[numParticles++] = p;
@@ -427,13 +366,14 @@ calc();
         window.addEventListener('mouseup', function(e) {
             press = false;
         }, false);
-        window.setInterval(frame, 1);
+        window.setInterval(frame, 1000/60);
         //window.setInterval(tick, 1);
-       // window.setInterval(calc, 1);
+        window.setInterval(calc, 1);
     };
 })();
 
 var Particle = function(x, y) {
+    //this.number=particles.length;
     this.x = x;
     this.y = y;
     this.gx = 0;
@@ -448,6 +388,10 @@ var Particle = function(x, y) {
 
 };
 Particle.prototype = {
+    drag: function() {
+        this.vx = this.vx * 0.99;
+        this.vy = this.vy * 0.99;
+    },
     move: function() {
         var _y1 = 0;
         var _x1 = 0;
@@ -468,24 +412,19 @@ Particle.prototype = {
                     }*/
         _x1 = mouseX - this.x;
         _y1 = mouseY - this.y;
-        _d = 0.5 / (_x1 * _x1 + _y1 * _y1);
-        if ((_x1 * _x1 + _y1 * _y1) > 312.5) {
-            //this.vx += _x1 * _d;
-            //this.vy += _y1 * _d;
-        }
-        _x1 = w / 2 - this.x;
-        _y1 = h / 2 - this.y;
+        /*_d = (10) / (_x1 * _x1 + _y1 * _y1);
+        if ((_x1 * _x1 + _y1 * _y1) < 1000*1000) {
+            this.vx += _x1 * _d;
+            this.vy += _y1 * _d;
+        }*/
 
-        _d = 0.5 / (_x1 * _x1 + _y1 * _y1);
-        if ((_x1 * _x1 + _y1 * _y1) > 312.5) {
-            //this.vx += _x1 * _d;
-            //this.vy += _y1 * _d;
-        }
+
         this.vy += SPH.GRAVITY;
         this.vx += this.fx; //+Math.random()*0.1-0.05;
         this.vy += this.fy; //+Math.random()*0.1-0.05;
         this.x += this.vx;
         this.y += this.vy;
+
         if (this.x < 10)
             this.vx += (10 - this.x) * 0.5 - this.vx * 0.5;
         if (this.y < 10)
